b56e19efc973e3466f4c98d2a837e3c2d716774a
Yaroslav <yarofeel@gmail.com>
2025-08-10 14:54:43 +0300

    check again =1

--- src/utils.py
+++ src/utils.py
@@ -2,4 +2,4 @@ from  config import PATH_TO_RESOURCES
 
 def load_messages_for_bot(name: str) -> str:
     with open(PATH_TO_RESOURCES / f"{name}.txt", encoding="utf-8") as file:
-        return file.read()
+        return file.read()1
