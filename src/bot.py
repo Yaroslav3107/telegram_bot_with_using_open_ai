b96ae719a90eded0cb1e36d97834f416d0392f67
Yaroslav <yarofeel@gmail.com>
2025-08-10 14:55:51 +0300

    check again = 2

--- src/bot.py
+++ src/bot.py
@@ -6,7 +6,7 @@ from utils import load_messages_for_bot
 
 async def start(update: Update, context: ContextTypes):
     text = load_messages_for_bot("main")
-    await update.message.reply_text(text1)
+    await update.message.reply_text(text)
 
 
 app = ApplicationBuilder().token(TG_BOT_API_KEY).build()
